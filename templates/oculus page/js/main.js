3bece903f6d65fcd7828705314ef8fe57459d7f8
RavilSunagatullin <121796856+RavilSunagatullin@users.noreply.github.com>
2023-04-08 21:02:04 +0300

    update

--- templates/oculus page/js/main.js
+++ templates/oculus page/js/main.js
@@ -1,3 +1,23 @@
-$(function(){
+'use strict'
 
-});
+const tabItem = document.querySelectorAll('.tabs__buttons-item')
+const tabContent = document.querySelectorAll('.tabs__content-item')
+
+tabItem.forEach(function(element){
+    element.addEventListener('click', open)
+})
+
+function open(event){
+    const tabTarget = event.currentTarget
+    const button = tabTarget.dataset.button
+
+    tabItem.forEach(function(item){
+        item.classList.remove('tabs__buttons-item--active')
+    })
+
+    tabContent.forEach(function(item){
+        item.classList.remove('tabs__content-item--active')
+    })
+    tabTarget.classList.add('tabs__buttons-item--active')
+    document.querySelector(`#${button}`).classList.add('tabs__content-item--active')
+}
